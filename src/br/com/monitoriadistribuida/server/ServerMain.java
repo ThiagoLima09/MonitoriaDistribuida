da85b65fd4c54a45aa187b97c99abd84fae931a7
Gabr1elFraga <gabrielsilveirafraga@hotmail.com>
2026-07-13 19:55:03 -0300

    servidor: controlar sessoes e proteger comandos autenticados

--- src/br/com/monitoriadistribuida/server/ServerMain.java
+++ src/br/com/monitoriadistribuida/server/ServerMain.java
@@ -1,5 +1,6 @@
 package br.com.monitoriadistribuida.server;
 
+import br.com.monitoriadistribuida.server.database.ConexaoBancoDados;
 import br.com.monitoriadistribuida.server.service.MonitorService;
 import br.com.monitoriadistribuida.server.service.UserService;
 
@@ -13,9 +14,20 @@ public class ServerMain {
 
     public static void main(String[] args) {
 
-        UserService userService = new UserService();
+        UserService userService;
+
+        try {
+            userService = new UserService();
+            System.out.println("Banco de dados conectado: " + ConexaoBancoDados.getUrl());
+        } catch (IllegalStateException e) {
+            System.out.println("Erro ao iniciar persistência de usuários: " + e.getMessage());
+            System.out.println("Verifique se o MySQL está ativo e se o schema monitoria existe.");
+            return;
+        }
 
         MonitorService monitorService = new MonitorService();
+
+        SessionManager sessionManager = new SessionManager();
 
         System.out.println("Iniciando servidor central...");
 
@@ -29,7 +41,7 @@ public class ServerMain {
 
                 System.out.println("Novo cliente conectado: " + clientSocket.getInetAddress());
 
-                ClientHandler clientHandler = new ClientHandler(clientSocket, userService, monitorService);
+                ClientHandler clientHandler = new ClientHandler(clientSocket, userService, monitorService, sessionManager);
 
                 Thread threadCliente = new Thread(clientHandler);
 
